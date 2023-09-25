afb874f1d50a23152b6193d45b49325476dea7b8
Nicole Romero <romeronicoleml@gmail.com>
2023-09-25 20:45:56 -0300

    rutas y useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,49 @@
-// import { Navbar } from './Navbar';
 import './App.css'
 import Navbar from './Navbar'
-// import MiComponent from './MiComponente';
-// import Titulo from './Titulo'
-// import Children from './Children';
-// import { useState } from 'react'
 import Formulario from './Formulario';
-import {Routes, Route} from 'react-router-dom'
-import {Link} from 'react-router-dom'
+import { Route, Routes, Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 
 
 function App() {
-  // const [fecha, setFecha] = useState('11 de septiembre');
-  // const [count, setCount] = useState(0)
-  // function cambiarFecha() {
-  //   setFecha('12 de septiembre')
-  // }
-  // const funSuma = num => {
-  //   return num + 2
-  // }
-  // const title = "Hola amigos soy un texto de props";
-  // const content = "soy otro texto props"
+  const [clicks, setClicks] = useState(0)
+  const [test, setTest] = useState('hola')
 
+
+  // useEffect(() => {
+  //   console.log('me acabo de ejecutar')
+  // })
+  //!EJECUTA EL CODIGO QUE TIENE ADENTRO, EN EL MONTAJE Y EN CADA ACTUALIZACION DE ESTADO
+  // useEffect(() => {
+  //   console.log('me acabo de ejecutar')
+  // },[test], [clicks])
+  //!EJECUTA EL CODIGO QUE TIENE DENTRO, EN EL MONTAJE Y EN CADA ACTIALIZACION EN PARTICULAR, (SEGUN SE LO INDIQUEMOS CON EL ESTADO)
+
+
+  useEffect(() => {
+    console.log('me acabo de montar');
+    return() => {
+      console.log('me acabo de desmontar')
+    }
+  }, [clicks])
   return (
     <>
 
-<Routes>
-  <Route path='/contacto' element={Navbar}/>
-  <Route path='/sobreNosotros' element={Formulario}/>
-</Routes>
-<Link to="/">Inicio</Link>
-<Link to='/contacto'>Contacto</Link>
-<Link to='/sobreNosotros' element={Formulario}>Formulario</Link>
-<h1>hola desde el app</h1>
-      {/* <Formulario /> */}
+      <div>
+      <h1>hola desde el app</h1>
+      <button onClick={() => setClicks(clicks + 1)}>{clicks}</button>
+      <button onClick={() => setTest('chau')}>{test}</button>
+      </div>
 
-      {/* <MiComponent/> */}
-      {/* <Children title={title} content={content} sum={funSuma}/> */}
-      {/* <button onClick={ () => setCount(count + 1)}>{count} </button> */}
-      {/* <h1>{fecha}</h1>
-      <button onClick={cambiarFecha} variant='danger'>Click aqui</button> */}
 
+   {/* <Routes> */}
+        {/* <Route path='/contacto' Component={Navbar} />
+        <Route path='/formulario' Component={Formulario} />
+        <Route path='/componente' component={Titulo}/> */}
+        {/* </Routes> */}
+        {/* <Link to="/formulario">Formulario de contacto</Link>
+        <Link to="/contacto">Contacto</Link> */}
     </>
   )
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,20 +1,34 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-
-function Navbar() {
-    // javascript
-
-    return(
-<>
-<div>
-    <h1>
-        navbar
-    </h1>
-    <a href="">contacto</a> 
-    <a href="">sobre nosotros</a>
-    <a href="">trabaja con nosotros</a>
-</div>
-</>
-    )
+function Navegacion() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">for,mulario</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Navbar;
+export default Navegacion;
